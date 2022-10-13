377ca5a3c969b41021f39075bc7263b07965882a
Khalif M <102165646+khalifmuham@users.noreply.github.com>
2022-10-12 22:34:18 -0400

    Add files via upload

---
+++ book_broker/src/main/java/com/khalifm/bookbroker/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.khalifm.bookbroker.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.khalifm.bookbroker.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+ 
+ Optional<User> findByEmail(String email);
+ 
+ List<User>findAll();
+ 
+}
---
+++ book_broker/src/test/java/com/khalifm/bookbroker/BookBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalifm.bookbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
